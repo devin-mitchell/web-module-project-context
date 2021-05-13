62f163fb7230fc1af3cfeac357c66711880ebf00
devin mitchell <devin.a.mitchell@gmail.com>
2021-05-13 16:56:53 -0600

    finished MVP and first stretch

--- src/App.js
+++ src/App.js
@@ -7,27 +7,46 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//contexts
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([
+			...cart, item
+		]);
 	};
 
+	const removeItem = id => {
+		setCart(
+			cart.filter(item => {
+				return(
+					item.id !== id
+				)
+			})
+		)
+	}
+
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={{ cart, removeItem }}>
+				<div className="App">
+					<Navigation />
+					{/* Routes */}			
+					<Route exact path="/">
+						<Products />
+					</Route>
+					<Route path="/cart">
+						<ShoppingCart/>
+					</Route>
+				</div>
+			</CartContext.Provider>
+		</ProductContext.Provider>
+		
 	);
 }
 
